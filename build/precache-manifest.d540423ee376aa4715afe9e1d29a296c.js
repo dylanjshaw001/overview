af29154e07ec9390a0acd261a3b93c23dc6af82d
Dylan <dylanjshaw001@gmail.com>
2021-12-21 23:06:30 -0500

    adding build folder to git history

---
+++ build/precache-manifest.d540423ee376aa4715afe9e1d29a296c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65904911b70d5b4a9da5f79b063d956c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e9d9534c93f314c5081",
+    "url": "/static/css/main.f7283fcb.chunk.css"
+  },
+  {
+    "revision": "357cc29a7b414cad2d04",
+    "url": "/static/js/2.c8b29a13.chunk.js"
+  },
+  {
+    "revision": "affe6ea79de343ee7d11c9df96679a08",
+    "url": "/static/js/2.c8b29a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9d9534c93f314c5081",
+    "url": "/static/js/main.8b46ba43.chunk.js"
+  },
+  {
+    "revision": "03718d45e8cc12431e7f",
+    "url": "/static/js/runtime-main.a8e87772.js"
+  },
+  {
+    "revision": "9f7408ab49f0472c998c5045416fb729",
+    "url": "/static/media/boston_b.9f7408ab.png"
+  },
+  {
+    "revision": "94a96a4a0735acad28b4996ffffc95ac",
+    "url": "/static/media/face_hover.94a96a4a.jpeg"
+  },
+  {
+    "revision": "4a74df67ecb21db0dee8c9db7e2b5a58",
+    "url": "/static/media/face_professional.4a74df67.jpeg"
+  },
+  {
+    "revision": "ab61e7afba10c65216dcb30a49e6f1eb",
+    "url": "/static/media/nyc-apple.ab61e7af.png"
+  },
+  {
+    "revision": "f0fde1718b5176b47726bd2188b6f507",
+    "url": "/static/media/sd_tree.f0fde171.png"
+  }
+]);
